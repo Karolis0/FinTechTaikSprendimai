5a9410f3fd939693c0262419241efea0b7e48cde
Karolis0 <36989486+Karolis0@users.noreply.github.com>
2022-03-04 17:01:12 +0200

    Added button to design and changing text on click

--- app/src/main/java/com/example/fintechtaiksprendimai/MainActivity.java
+++ app/src/main/java/com/example/fintechtaiksprendimai/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.fintechtaiksprendimai;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buttonText = (Button) findViewById(R.id.btnInputText);
+        buttonText.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                TextView tv = (TextView) findViewById(R.id.txtTextDisplay);
+                tv.setText("Labas rytas");
+            }
+        });
     }
 }
